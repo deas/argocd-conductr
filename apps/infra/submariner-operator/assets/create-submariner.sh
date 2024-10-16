0aa8d1333b8a960df6b7702288afdfe3e8a439e2
Andreas Steffan <a.steffan@contentreich.de>
2024-10-16 19:55:06 +0200

    wip

--- apps/infra/submariner-operator/assets/create-submariner.sh
+++ apps/infra/submariner-operator/assets/create-submariner.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+set -xe
+
+TEMPLATE=$1
+
 export BROKER_NS=submariner-k8s-broker
 export SUBMARINER_NS=submariner-operator
 
@@ -8,3 +12,5 @@ export SUBMARINER_NS=submariner-operator
 export SUBMARINER_BROKER_TOKEN=$(kubectl -n "${BROKER_NS}" get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='${BROKER_NS}-client')].data.token}" | base64 --decode)
 export SUBMARINER_BROKER_CA=$(kubectl -n ${BROKER_NS} get secrets -o json | jq -r -c '[.items[] | select(.metadata.annotations."kubernetes.io/service-account.name"=="submariner-k8s-broker-client") | select(.data."ca.crt" != null)] | .[0].data."ca.crt"' | base64 --decode)
 export SUBMARINER_BROKER_URL=$(kubectl -n default get endpoints kubernetes -o jsonpath="{.subsets[0].addresses[0].ip}:{.subsets[0].ports[?(@.name=='https')].port}")
+
+cat "${TEMPLATE}" | envsubst | kubectl -n "${SUBMARINER_NS}" apply -f -
