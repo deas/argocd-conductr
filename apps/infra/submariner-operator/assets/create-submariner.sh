4b89e2d8774199d51662ffd2d523d065a66a281c
Andreas Steffan <a.steffan@contentreich.de>
2024-10-16 22:44:49 +0200

    wip

--- apps/infra/submariner-operator/assets/create-submariner.sh
+++ apps/infra/submariner-operator/assets/create-submariner.sh
@@ -14,5 +14,4 @@ export SUBMARINER_BROKER_CA=$(kubectl -n ${BROKER_NS} get secrets -o json | jq -
 
 export SUBMARINER_BROKER_URL=$(kubectl -n default get endpoints kubernetes -o jsonpath="{.subsets[0].addresses[0].ip}:{.subsets[0].ports[?(@.name=='https')].port}")
 
-cat "${TEMPLATE}" | envsubst
-# | kubectl -n "${SUBMARINER_NS}" apply -f -
+cat "${TEMPLATE}" | envsubst | tee | kubectl -n "${SUBMARINER_NS}" apply -f -
