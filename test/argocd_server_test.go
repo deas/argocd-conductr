127c01c2d1aa50fbb678f9da0bb4c768be18ba75
Andreas Steffan <a.steffan@contentreich.de>
2025-05-24 13:23:03 +0200

    wip

--- test/argocd_server_test.go
+++ test/argocd_server_test.go
@@ -29,7 +29,7 @@ var _ = Describe("Argo Server Deployment", func() {
 	})
 
 	It("should have the desired number of replicas", func() {
-		deploymentName := "argo-server"
+		deploymentName := "argocd-server"
 		namespace := "argocd"
 
 		// Get the deployment
